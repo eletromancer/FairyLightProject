a27af8329312e401033f06c4996ed5f78d15148f
Luis Henrique Silva <120187840+eletromancer@users.noreply.github.com>
2023-08-26 19:07:08 -0300

    Add files via upload

---
+++ FairyLight.ino
@@ -0,0 +1,26 @@
+const int  pinSeven = 7;
+const int  pinSix = 6;
+const int pinFive = 5;
+
+void setup() {
+
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(pinSeven, HIGH);
+  delay(50);
+  digitalWrite(pinSix, HIGH);
+  delay(50);
+  digitalWrite(pinFive, HIGH);
+  delay(50);
+  
+  digitalWrite(pinSeven, LOW);
+  digitalWrite(pinSix, LOW);
+  digitalWrite(pinFive, LOW);
+  delay(50);
+  
+}
